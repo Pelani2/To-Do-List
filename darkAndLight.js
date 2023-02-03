7721fad0978fdece98764ac8fe28fd41fd51bc6c
Župunski Igor <116688664+Pelani2@users.noreply.github.com>
2023-02-03 15:29:41 +0100

    Add files via upload
    added some functionality to light mode button

--- darkAndLight.js
+++ darkAndLight.js
@@ -64,3 +64,100 @@
         }
     }
 }
+
+// creating a function for lightModeButton
+{
+    // light mode color variables
+    const darkGrayishGreen = "#3F403B";
+    const mediumGray = "#727365";
+    const lightGrayishYellow = "#BFBFAE";
+    const veryLightGrayishYellow = "#F2F2E4";
+    const darkGrey = "#262626";
+
+    const lightButton = document.getElementById("lightModeButton");
+    lightButton.addEventListener("click", function() {
+        // styling the frame
+        const frame = document.getElementById("frame");
+        frame.style.backgroundColor = veryLightGrayishYellow;
+
+        // styling the frame title
+        const frameTitle = document.getElementById("frameTitle");
+        frameTitle.style.color = mediumGray;
+
+        // styling the addTaskButton
+        const addTaskButton = document.getElementById("addTaskButton");
+        addTaskButton.style.backgroundColor = lightGrayishYellow;
+        addTaskButton.style.color = mediumGray;
+        addTaskButton.addEventListener("mouseenter", function() {
+            this.style.backgroundColor = darkGrey;
+            this.style.color = veryLightGrayishYellow;
+        });
+        addTaskButton.addEventListener("mouseleave", function() {
+            this.style.backgroundColor = lightGrayishYellow;
+            this.style.color = mediumGray;
+        });
+
+        // styling the deleteAllTasksButton
+        const deleteAllTasksButton = document.getElementById("deleteAllTasksButton");
+        deleteAllTasksButton.style.backgroundColor = lightGrayishYellow;
+        deleteAllTasksButton.style.color = mediumGray;
+        deleteAllTasksButton.addEventListener("mouseenter", function() {
+            this.style.backgroundColor = darkGrey;
+            this.style.color = veryLightGrayishYellow;
+        });
+        deleteAllTasksButton.addEventListener("mouseleave", function() {
+            this.style.backgroundColor = lightGrayishYellow;
+            this.style.color = mediumGray;
+        });
+
+        // styling the taskHolder
+        const taskHolder = document.getElementById("taskHolder");
+        taskHolder.style.border = "2px solid #727365";
+
+        // styling the taskHolderTitle
+        const taskHolderTitle = document.getElementById("taskHolderTitle");
+        taskHolderTitle.style.color = mediumGray;
+
+        // styling the taskContent
+        const taskContent = document.getElementById("taskContent");
+        taskContent.style.color = darkGrey;
+        taskContent.style.backgroundColor = lightGrayishYellow;
+
+        // styling the buttons inside buttonHolder
+        const buttonHolder = document.getElementById("buttonHolder");
+        const buttons = buttonHolder.children;
+        for (var q = 0; q < buttons.length; q++) {
+            buttons[q].style.color = mediumGray;
+            buttons[q].style.backgroundColor = lightGrayishYellow;
+            buttons[q].addEventListener("mouseenter", function() {
+                this.style.backgroundColor = darkGrey;
+                this.style.color = veryLightGrayishYellow;
+            });
+            buttons[q].addEventListener("mouseleave", function() {
+                this.style.color = mediumGray;
+                this.style.backgroundColor = lightGrayishYellow;
+            });
+        }
+
+        // styling the colorModeHolder
+        const colorModeHolder = document.getElementById("colorModeHolder");
+        colorModeHolder.style.backgroundColor = lightGrayishYellow;
+        colorModeHolder.style.color = mediumGray;
+
+        // styling the anchors inside the dropdown menu
+        const dropdownMenu = document.querySelector(".dropdown-menu");
+        const anchors = dropdownMenu.children;
+        for (var q = 0; q < anchors.length; q++) {
+            anchors[q].style.backgroundColor = lightGrayishYellow;
+            anchors[q].style.color = mediumGray;
+            anchors[q].addEventListener("mouseenter", function() {
+                this.style.backgroundColor = darkGrey;
+                this.style.color = veryLightGrayishYellow;
+            });
+            anchors[q].addEventListener("mouseleave", function() {
+                this.style.backgroundColor = lightGrayishYellow;
+                this.style.color = mediumGray;
+            });
+        }
+    });
+}
